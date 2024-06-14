a86eefa19a8a13bac3966bee059d05c5adf2556f
Dasol Kim <zoocanon92@gmail.com>
2024-06-15 01:45:18 +0900

    회원 가입, 회원 목록 API와 템플릿 추가

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,8 +1,7 @@
 package jpabook.jpashop.domain;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.ArrayList;
 import java.util.List;
